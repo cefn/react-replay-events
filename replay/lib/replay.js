722cf87a989dbf3a5101d67d1ae2dd7b3c25a732
Cefn Hoile <cefn.hoile@bbc.co.uk>
2020-09-01 12:03:18 +0100

    Simplify. Remove IE cases

--- replay/lib/replay.js
+++ replay/lib/replay.js
@@ -6,53 +6,21 @@ const sleep = ms => {
   })
 }
 
-function copyForInternetExplorer(fromEvent, toEvent) {
-  const propNames = [
-    'type',
-    'bubbles',
-    'cancelable',
-    'view',
-    'detail',
-    'screenX',
-    'screenY',
-    'clientX',
-    'clientY',
-    'ctrlKey',
-    'altKey',
-    'shiftKey',
-    'metaKey',
-    'button',
-    'relatedTarget'
-  ];
-  for (const propName of propNames) {
-    toEvent[propName] = fromEvent[propName];
-  }
-}
-
-function cloneMouseEvent(reactEvent, legacyMode = true, internetExplorer = false) {
-  if (legacyMode) {
-    //this seems to work in Chrome, Firefox, Safari
-    const cloneEvent = document.createEvent("MouseEvent"); // should this be PointerEvent on IE/Others? 
-    if (internetExplorer) copyForInternetExplorer(reactEvent.nativeEvent, cloneEvent);
-    cloneEvent.initEvent('click', true, false);
-    cloneEvent.stopPropagation(); //allow default event, don't notify listeners
-    return cloneEvent;
-  }
-  else {
-    //this doesn't work anywhere, but maybe I made a mistake
-    const nativeEvent = reactEvent.nativeEvent;
-    const cloneEvent = new MouseEvent(nativeEvent.type, nativeEvent);
-    return cloneEvent;
-  }
+function cloneMouseEvent(reactEvent) {
+  //this seems to work in Chrome, Firefox, Safari
+  const cloneEvent = document.createEvent("MouseEvent"); // should this be PointerEvent on IE/Others? 
+  cloneEvent.initEvent('click', true, false);
+  cloneEvent.stopPropagation(); //allow default event, don't notify listeners
+  return cloneEvent;
 }
 
 export function ReplayAncestor({ children }) {
 
   const [completed, setCompleted] = useState(null);
 
-  const completeTask = async () => {
+  const sendToAti = async () => {
     setCompleted(false);
-    await sleep(2000);
+    await sleep(50);
     setCompleted(true);
   }
 
@@ -62,9 +30,9 @@ export function ReplayAncestor({ children }) {
         reactEvent.persist();
         reactEvent.preventDefault(); //prevent default event, but continue to notify listeners
         //handle case that preventDefault called by a later listener?
-        const cloneEvent = cloneMouseEvent(reactEvent, true);
+        const cloneEvent = cloneMouseEvent(reactEvent);
         cloneEvent.alreadyPaused = true;
-        await completeTask()
+        await sendToAti() //pretends to be ATI
         const cancelled = !reactEvent.target.dispatchEvent(cloneEvent);
       }
     }
